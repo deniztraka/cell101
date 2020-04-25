990f4e82e4c17b87d46de8f28a821c5dce972bdd
deniztraka <deniz.traka@gmail.com>
2020-04-26 01:19:34 +0300

    environment

---
+++ Assets/DenizTraka/SimpleCharacter/Scripts/Behaviours/Items/Environment/TorchBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine;
+namespace DTWorld.Behaviours.Items.Environment
+{
+    public class TorchBehaviour : MonoBehaviour
+    {
+        public Light2D Light;
+        public float MaxIntensity;
+        public float MinIntensity;
+        public float frequency = .1f;
+        public float timePassed = 0;
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void LateUpdate()
+        {
+            if (timePassed > frequency && Random.Range(0, 10) > 5)
+            {
+                timePassed = 0;
+                Light.intensity = Random.Range(MinIntensity, MaxIntensity);
+            }
+            timePassed += Time.deltaTime;
+        }
+    }
+}
