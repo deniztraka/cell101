0e955b21659255e5e1920a171b7e0aad10afec33
deniztraka <deniz.traka@gmail.com>
2020-05-02 17:05:40 +0300

    Simple Menu

--- Assets/DenizTraka/SimpleCharacter/Scripts/Behaviours/Utils/GameManager.cs
+++ Assets/DenizTraka/SimpleCharacter/Scripts/Behaviours/Utils/GameManager.cs
@@ -1,13 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using DTWorld.Behaviours.Interfacelike;
+using DTWorld.Behaviours.Mobiles;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    HealthBehaviour playerHealth;
+    public GameObject OnDeathCanvas;
     // Start is called before the first frame update
     void Start()
     {
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<HealthBehaviour>();
+        playerHealth.OnHealthBelowZeroEvent += new HealthBehaviour.OnHealthBelowZeroEventHandler(OnDeath);
         //Application.targetFrameRate = 30;
+    }
+
+    private void OnDeath()
+    {
+        OnDeathCanvas.SetActive(true);
     }
 
     // Update is called once per frame
