a05ba43fa1cac736074505b358ec82991f283a2e
deniztraka <deniz.traka@gmail.com>
2020-06-14 00:52:58 +0300

    devamke

--- Assets/DenizTraka/SimpleCharacter/Scripts/Behaviours/Items/Environment/TorchBehaviour.cs
+++ Assets/DenizTraka/SimpleCharacter/Scripts/Behaviours/Items/Environment/TorchBehaviour.cs
@@ -4,22 +4,32 @@ using UnityEngine.Experimental.Rendering.Universal;
 using UnityEngine;
 namespace DTWorld.Behaviours.Items.Environment
 {
+    [ExecuteInEditMode]
     public class TorchBehaviour : MonoBehaviour
     {
         public Light2D Light;
         public float MaxIntensity;
         public float MinIntensity;
-        public float frequency = .1f;
-        public float timePassed = 0;
+        public float Frequency = .1f;
+        private float timePassed = 0;
+        public float yOffset;
+        private Transform child;
         void Start()
         {
+            child = transform.GetChild(0);
+            child.transform.parent = transform;
 
+        }
+
+        void Update()
+        {
+            child.transform.localPosition = new Vector2(0, yOffset);
         }
 
         // Update is called once per frame
         void LateUpdate()
         {
-            if (timePassed > frequency)
+            if (timePassed > Frequency)
             {
                 timePassed = 0;
                 Light.intensity = Random.Range(MinIntensity, MaxIntensity);
